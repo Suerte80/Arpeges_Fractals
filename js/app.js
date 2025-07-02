52da21ccda343923bbe852804be0011fd25b9839
Suerte80 <alexiscourtin3@gmail.com>
2025-07-02 15:57:29 +0200

    Simplication de App.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const modalHisto = document.getElementById("modal-histo");
     const buttonHisto = document.getElementById("button-histo");
     const buttonSend = document.getElementById("button-send");
+    const buttonClear = document.getElementById("button-clear")
 
     // Fleche
     const arrow = document.getElementById("arrow");
@@ -17,32 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const mostViewedArticles = [];
     const fullListArticles = [];
 
-    // On vérifie que les éléments sont présent dans la page.
-    if( fullArticlescontainer || newArticlesContainer || mostViewedArticlesContainer ){
-        fetch("articles/articles.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Erreur lors du chargement des articles");
-            }
-            return response.json();
-        }) .then(data => {
-
-            data.forEach(article => {
-                if( article.new ){
-                    newArticles.push(article);
-                } else if( article.mostViewed ){
-                    mostViewedArticles.push(article);
-                }
-                
-                const htmlArticle = addArticle(article, newArticlesContainer, mostViewedArticlesContainer, fullArticlescontainer);
-                callbackModalManager(htmlArticle, modalContainer, article)
-                fullListArticles.push(article, modalContainer);
-            });
-
-        }) .catch(error => {
-            console.error("Erreur lors du chargement des articles :", error);
-        });
-    }
+    handleLoadArticle(fullArticlescontainer,
+        newArticlesContainer,
+        mostViewedArticlesContainer,
+        modalContainer
+    );
     
     // Fermeture de la modal lors du click de la souris
     if( modalContainer ){ // On vérifie qu'on a bien la modal de disponible
@@ -62,91 +42,19 @@ document.addEventListener("DOMContentLoaded", () => {
     // Section pour le bouton historique de la page contact
     if( modalHisto && buttonHisto ){
         buttonHisto.addEventListener("click", () => {
-            // On récupére l'historique
-            const arrMessages = MessageManager.loadMessages();
-
-            // On récupére le ul
-            const htmlUL = modalHisto.querySelector("ul");
-            
-            // On récupére le titre de la modal
-            const titleHistoModal = modalHisto.querySelector("h2");
-
-            // ON néttoie le fils de htmlUL
-            removeChildren(htmlUL);
-
-            if( arrMessages.length > 0 ){
-                titleHistoModal.textContent = "Historique";
-
-                arrMessages.forEach(message => {
-                    createHistoListItem(htmlUL, message);       
-                });
-            } else{
-                titleHistoModal.textContent = "Aucun Historique";
-            }
-
-            // On ouvre la modal
-            modalHisto.classList.remove("hidden");
+            callBackHistoryOnClick(modalHisto);
         });
 
-        // On récupére le bouton
-        const closeModalButton = modalHisto.querySelector("#close-modal");
-        // On ajoute le callback sur le click du bouton
-        closeModalButton.addEventListener("click", () => {
-            modalHisto.classList.add("hidden");
+        buttonClear.addEventListener("click", () => {
+            MessageManager.clearMessages();
+
+            // Faire appel a la fonction pour rafraichir l'historique.
+            callBackHistoryOnClick(modalHisto);
         });
     }
     
     // Section pour le bouton envoyer de la page contact
-    if( buttonSend ){
-        const emailForm = document.getElementById("form-email");
-        const objectForm = document.getElementById("form-objet");
-        const messageForm = document.getElementById("form-message");
+    handleFormSubmission(modalHisto, buttonSend);
 
-        buttonSend.addEventListener("click", (e) => {
-            e.preventDefault();
-
-            let isErrorOnForm = false;
-
-            // Vérification des inputs
-            const email = emailForm.value;
-            if( email === "" && !validateEmail(email)){
-                // Ici on trigger la notification
-                createNotification("Email invalide", 2000, notificationMessageTypes.ERROR);
-                toggleInvalidityForm(emailForm, 2000);
-                isErrorOnForm = true;
-            }
-
-            const object = objectForm.value;
-            if( object === "" ){
-                // Ici on trigger la notif
-                createNotification("Objet vide", 2000, notificationMessageTypes.ERROR);
-                toggleInvalidityForm(objectForm, 2000);
-                isErrorOnForm = true;
-            }
-
-            const message = messageForm.value;
-            if( message === "" ){
-                // Ici on trigger la notif.
-                createNotification("Message vide", 2000, notificationMessageTypes.ERROR);
-                toggleInvalidityForm(messageForm, 2000);
-                isErrorOnForm = true;
-            }
-
-            if(isErrorOnForm) return;
-
-            MessageManager.save(email, object, message);
-
-            // Ici on trigger la notif
-            createNotification("Message envoyé", 5000, notificationMessageTypes.INFO);
-        })
-    }
-
-    if( arrow ){
-        arrow.addEventListener("click", () => {
-            const target = document.getElementById("latest-articles");
-            if(target){
-                target.scrollIntoView({behavior: "smooth"});
-            }
-        });
-    }
+    handleArrowScrollDown(arrow);
 });
